e56184ca90709841e4a5700076e01ba850a74c1e
unknown <s.ghatak2019@gmail.com>
2025-09-06 21:23:00 +0530

    2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
+
+import express,{Request,Response} from "express";
+const app = express();
 const sayHello = (name: string): void => {
   console.log(`Hello, ${name}!`);
 };
 
 sayHello("Supratik");
 console.log("Server is running...");
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;      
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
