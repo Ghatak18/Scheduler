e26aa9100e28938a9ed530d641b9cb4f636f081f
unknown <s.ghatak2019@gmail.com>
2025-09-06 21:56:36 +0530

    knex added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 
 import express,{Request,Response} from "express";
+import dotenv from "dotenv";
+import 'dotenv/config';
+import db from './db.ts';
+
+// dotenv.config();
 const app = express();
 const sayHello = (name: string): void => {
   console.log(`Hello, ${name}!`);
@@ -7,12 +12,15 @@ const sayHello = (name: string): void => {
 
 sayHello("Supratik");
 console.log("Server is running...");
-
+db();
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello World!");
 });
+//console.log('All env variables:', process.env);
+const port1 =  process.env.MY_NAME;
+console.log("Port from env:", port1);
+const PORT = process.env.PORT || 3000;  
 
-const PORT = process.env.PORT || 3000;      
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
